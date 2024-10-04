ee03a9d15e2c27d76b80961ddd86e8bd8dd567f1
sithum20210380 <85311920+sithum20210380@users.noreply.github.com>
2024-10-04 16:35:21 +0530

    Initial Commit

---
+++ todoAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todoAPP
+//
+//  Created by Sithum Raveesha on 2024-10-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todoAPP/todoAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAPPApp.swift
+//  todoAPP
+//
+//  Created by Sithum Raveesha on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct todoAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
